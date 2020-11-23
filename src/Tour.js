9580de048da07ff434e76efabc47e636fb3bb6ab
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-11-23 14:14:26 +0000

    Show more text to read on click

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Tour({ id, image, info, price, name }) {
+  const [readMore, setReadMore] = useState(false);
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -9,6 +10,13 @@ function Tour({ id, image, info, price, name }) {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? 'Show Less' : 'Read More'}
+          </button>
+        </p>
+        <button className="delete-btn">not interested</button>
       </footer>
     </article>
   );
