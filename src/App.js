acf96c81b0871c6dfa72114a29b6fcddd3cdd5a6
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-11-23 13:50:18 +0000

    use the UseState hook to set the state of the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+const url = 'https://course-api.netlify.app/api/react-tours-project';
 
 function App() {
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => console.log('response is ', res));
+  });
+  const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>App</h1>
     </div>
   );
 }
